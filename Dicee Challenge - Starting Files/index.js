fc3197d671bc15892d497c636574d87281aaad75
z297xu <kyrie807782352@gmail.com>
2023-09-24 18:16:56 -0400

    change the img to random number 2

--- Dicee Challenge - Starting Files/index.js
+++ Dicee Challenge - Starting Files/index.js
@@ -1,6 +1,15 @@
+/* Change the dice 1 */
 var randomNumber1 = Math.floor(Math.random() * 6) + 1
 // console.log(randomNumber1)
 
 var src = "./images/dice" + randomNumber1 +".png";
 
 document.querySelector(".dice > .img1").setAttribute("src", src);
+
+
+/* Change the dice 2*/
+var randomNumber2 = Math.floor(Math.random() * 6) + 1
+
+src = "./images/dice" + randomNumber2 +".png";
+
+document.querySelector(".dice > .img2").setAttribute("src", src);
