a2be970051992bb7c20d96a7f5653b3b507c692d
z297xu <kyrie807782352@gmail.com>
2023-09-24 18:21:05 -0400

    Change the title to display a winner with flag

--- Dicee Challenge - Starting Files/index.js
+++ Dicee Challenge - Starting Files/index.js
@@ -13,3 +13,14 @@ var randomNumber2 = Math.floor(Math.random() * 6) + 1
 src = "./images/dice" + randomNumber2 +".png";
 
 document.querySelector(".dice > .img2").setAttribute("src", src);
+
+
+
+/* Change the Title to display a Winner*/
+if (randomNumber1 > randomNumber2){
+    document.querySelector("h1").innerHTML = "&#128681 Play 1 Wins! ";
+} else if (randomNumber1 < randomNumber2){
+    document.querySelector("h1").innerHTML = "Play 2 Wins! 	&#128681";
+} else {
+    document.querySelector("h1").innerHTML = "Draw!"
+}
