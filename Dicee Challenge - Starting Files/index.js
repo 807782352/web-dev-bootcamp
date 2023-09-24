c19196686a3f7f2134030fcac5ce53741ecea1b4
z297xu <kyrie807782352@gmail.com>
2023-09-24 18:15:36 -0400

    change the img to a random dice in dice 1

--- Dicee Challenge - Starting Files/index.js
+++ Dicee Challenge - Starting Files/index.js
@@ -1,2 +1,6 @@
 var randomNumber1 = Math.floor(Math.random() * 6) + 1
-console.log(randomNumber1)
+// console.log(randomNumber1)
+
+var src = "./images/dice" + randomNumber1 +".png";
+
+document.querySelector(".dice > .img1").setAttribute("src", src);
