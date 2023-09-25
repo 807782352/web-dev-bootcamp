577ac9a977e899387206291e5ae90fb02742c952
z297xu <kyrie807782352@gmail.com>
2023-09-25 16:39:47 -0400

    add animation and sound

--- Simon Game Challenge Starting Files/game.js
+++ Simon Game Challenge Starting Files/game.js
@@ -6,4 +6,9 @@ function nextSequence() {
     var randomNumber = Math.floor(Math.random() * 4); // 0 ~ 3
     var randomChosenColour = buttonColours[randomNumber];
     gamePattern.push(randomChosenColour);
+    $("#"+randomChosenColour).fadeOut(100).fadeIn(100);
+    var music = new Audio("./sounds/"+randomChosenColour+".mp3");
+    music.play();
 }
+
+
