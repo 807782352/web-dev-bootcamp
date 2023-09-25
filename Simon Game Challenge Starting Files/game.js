38a5cec3016bb90acf47422dbe1098030c3bb389
z297xu <kyrie807782352@gmail.com>
2023-09-25 17:14:12 -0400

    add sounds to button clicks

--- Simon Game Challenge Starting Files/game.js
+++ Simon Game Challenge Starting Files/game.js
@@ -4,19 +4,25 @@ const userClickedPattern = [];
 
 const buttonColours = ["red", "blue", "green", "yellow"];
 
-function nextSequence() {
-    var randomNumber = Math.floor(Math.random() * 4); // 0 ~ 3
-    var randomChosenColour = buttonColours[randomNumber];
-    gamePattern.push(randomChosenColour);
-    $("#"+randomChosenColour).fadeOut(100).fadeIn(100);
-    var music = new Audio("./sounds/"+randomChosenColour+".mp3");
-    music.play();
-}
-
 $(".btn").on("click", function(){
     // 哪个btn被点击，这个this就指向哪个btn 元素
     // console.log(this);
     var userChosenColour = this.id;
     userClickedPattern.push(userChosenColour);
+    playSound(userChosenColour);
     console.log(userClickedPattern);
 })
+
+function nextSequence() {
+    var randomNumber = Math.floor(Math.random() * 4); // 0 ~ 3
+    var randomChosenColour = buttonColours[randomNumber];
+    gamePattern.push(randomChosenColour);
+    $("#"+randomChosenColour).fadeOut(100).fadeIn(100);
+    playSound(randomChosenColour);
+}
+
+function playSound(name) {
+    var music = new Audio("./sounds/"+name+".mp3");
+    music.play();
+}
+
