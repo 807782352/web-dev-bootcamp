d95273333c4f4f842939cdea638658f2c4a3a0ca
z297xu <kyrie807782352@gmail.com>
2023-09-25 16:27:35 -0400

    adjust some codes positions

--- Simon Game Challenge Starting Files/game.js
+++ Simon Game Challenge Starting Files/game.js
@@ -2,10 +2,8 @@ const gamePattern = [];
 
 const buttonColours = ["red", "blue", "green", "yellow"];
 
-var randomChosenColour = buttonColours[nextSequence()];
-gamePattern.push(randomChosenColour);
-
 function nextSequence() {
     var randomNumber = Math.floor(Math.random() * 4); // 0 ~ 3
-    return randomNumber;
+    var randomChosenColour = buttonColours[randomNumber];
+    gamePattern.push(randomChosenColour);
 }
