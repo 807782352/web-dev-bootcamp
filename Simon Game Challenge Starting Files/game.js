fb81ff9809bef1eab009e3774668293ee3b4bcf8
z297xu <kyrie807782352@gmail.com>
2023-09-25 16:24:57 -0400

    create a new pattern

--- Simon Game Challenge Starting Files/game.js
+++ Simon Game Challenge Starting Files/game.js
@@ -1,1 +1,11 @@
-alert("hello, world!");
+const gamePattern = [];
+
+const buttonColours = ["red", "blue", "green", "yellow"];
+
+var randomChosenColour = buttonColours[nextSequence()];
+gamePattern.push(randomChosenColour);
+
+function nextSequence() {
+    var randomNumber = Math.floor(Math.random() * 4); // 0 ~ 3
+    return randomNumber;
+}
