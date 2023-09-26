856a27508c831c671b51fe76d856900cbeed04fd
z297xu <kyrie807782352@gmail.com>
2023-09-25 21:43:27 -0400

    check the user's answer against the game

--- Simon Game Challenge Starting Files/game.js
+++ Simon Game Challenge Starting Files/game.js
@@ -4,22 +4,22 @@ var level = 0;
 
 var started = false;
 
-const userClickedPattern = [];
+var userClickedPattern = [];
 
 const buttonColours = ["red", "blue", "green", "yellow"];
 
 $(".btn").on("click", function(){
-    // 哪个btn被点击，这个this就指向哪个btn 元素
     // console.log(this);
     var userChosenColour = this.id;
     userClickedPattern.push(userChosenColour);
     playSound(userChosenColour);
     // console.log(userChosenColour);
     animatePress(userChosenColour);
+
+    checkAnswer(userClickedPattern.length-1);
 })
 
 $(document).keypress(function(){
-
     // initialization
     if (!started){
         $("h1").text("Level " + level);
@@ -29,13 +29,26 @@ $(document).keypress(function(){
 });
 
 
+function checkAnswer(currentLevel){
+    if (gamePattern[currentLevel] === userClickedPattern[currentLevel]){
+        console.log("success");
+        if (userClickedPattern.length === gamePattern.length){
+            // meaning this level is done successfully
+            setTimeout(()=>{
+                nextSequence();
+            }, 1000);
+        }
+    } else {
+        console.log("wrong");
+    }
+}
 
 function nextSequence() {
+    // everrytime the function is triggered, need to reset the userClickedPattern
+    userClickedPattern = [];
 
     level++;
-
     $("h1").text("Level " + level);
-
     var randomNumber = Math.floor(Math.random() * 4); // 0 ~ 3
     var randomChosenColour = buttonColours[randomNumber];
     gamePattern.push(randomChosenColour);
