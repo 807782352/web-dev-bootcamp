f04946b606080fa492abd2dad4c17a31026ec792
z297xu <kyrie807782352@gmail.com>
2023-09-28 10:55:06 -0400

    update index3.js

--- Backend-Dev/3.4+Middleware/3.4 Middleware/index3.js
+++ Backend-Dev/3.4+Middleware/3.4 Middleware/index3.js
@@ -5,6 +5,13 @@ const port = 3000;
 
 app.use(logger);
 
+function logger(req, res, next){
+  console.log("Request method: ", req.method);
+  console.log("Request URL: ", req.url);
+  // 注：如果不写next()的话，后面的Hello是无法加载的，一直处于hanging中
+  next();
+}
+
 app.get("/", (req, res) => {
   res.send("Hello");
 });
