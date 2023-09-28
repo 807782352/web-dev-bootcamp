39546cbd9efdedead36bef622f41554deef5d5ea
z297xu <kyrie807782352@gmail.com>
2023-09-28 13:32:09 -0400

    add 3.5

--- Backend-Dev/3.4+Middleware/3.4 Middleware/index4.js
+++ Backend-Dev/3.4+Middleware/3.4 Middleware/index4.js
@@ -1,7 +1,32 @@
 import express from "express";
+import bodyParser from "body-parser";
+import {dirname} from "path";
+import { fileURLToPath } from "url";
+
 
 const app = express();
 const port = 3000;
+
+var bandName = "";
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.use((req, res, next) => {
+  console.log(req.body);
+  // req.
+  bandName = req.body["street"] + " "  + req.body["pet"];
+  next();
+})
+
+app.post("/submit", (req, res) => {
+  res.send(`<h1>Your band name is:</h1><h2>${bandName}✌️</h2>`);
+})
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/public/index.html");
+});
 
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
