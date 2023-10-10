9bff4380ff4a0573b5d6e1b0fd756ec34fe8edd6
z297xu <kyrie807782352@gmail.com>
2023-10-10 15:39:42 -0400

    finish delete requests

--- Backend-Dev/6.0+DIY+API/6.0 DIY API/index.js
+++ Backend-Dev/6.0+DIY+API/6.0 DIY API/index.js
@@ -69,7 +69,7 @@ app.patch("/jokes/:id", (req, res) => {
   const id = parseInt(req.params.id);
   const { text, type } = req.body;
 
-  const existingJoke = jokes.find(joke => joke.id === id);
+  const existingJoke = jokes.find((joke) => joke.id === id);
   const updateIdx = jokes.findIndex((joke) => joke.id === id);
 
   /*
@@ -95,8 +95,32 @@ app.patch("/jokes/:id", (req, res) => {
 });
 
 //7. DELETE Specific joke
+app.delete("/jokes/:id", (req, res) => {
+  const id = parseInt(req.params.id);
+
+  const deleteIdx = jokes.findIndex((joke) => joke.id === id);
+
+  if (deleteIdx > -1) {
+    jokes.splice(deleteIdx, 1);
+    res.status(200).json({ message: `joke is deleted successfully!` });
+  } else {
+    res.status(404).json({ error: `joke with id: ${id} is not found!` });
+  }
+});
 
 //8. DELETE All jokes
+app.delete("/all", (req, res) => {
+  const key = req.query.key;
+
+  if (key === masterKey) {
+    jokes = [];
+    res.sendStatus(200);
+  } else {
+    res
+      .status(404)
+      .json({ error: `You don't have authorization to delete the jokes!` });
+  }
+});
 
 app.listen(port, () => {
   console.log(`Successfully started server on port ${port}.`);
